464f5a6c67a00fd0179909b140ef35da7e50e5ca
Dev Patel <dvptl68@gmail.com>
2020-11-13 21:07:10 -0500

    add new document function

--- node/functions/index.js
+++ node/functions/index.js
@@ -1,9 +1,22 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+// Example cloud function
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// Creates a new document for each new user
+exports.createDoc = functions.auth.user().onCreate(user => {
+
+    // Data to be stored in document
+    const userData = {
+        name: user.displayName,
+        email: user.email
+    };
+
+    // Add user data to new document
+    return admin.firestore().doc(`users/${user.uid}`).set(userData);
+});
